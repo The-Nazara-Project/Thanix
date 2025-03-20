a363cb33a30bb155b272682dbf90d7420364a92d
Tiara Lena Hock <hock@b1-systems.de>
2025-03-20 16:06:31 +0100

    Add unittests for is_unsanitary in util.rs

--- src/util.rs
+++ src/util.rs
@@ -11,16 +11,57 @@
 /// To work around this, the `--workaround` flag was added to Thanix, which will check at struct
 /// generation, whether the struct is part of this **manually maintained list of troublemakers**
 ///
-/// > [!Note]
-/// > This list is maintained manually by the Nazara Team, as there is currently no real way to
+/// ** This list is maintained manually by the Nazara Team, as there is currently no real way to
 /// automate this.
-/// > If you have problems and need something to be added to it, please open a bug in our [issues
-/// section](https://github.com/The-Nazara-Project/Thanix/issues/).
+/// If you have problems and need something to be added to it, please open a bug in our [issues
+/// section](https://github.com/The-Nazara-Project/Thanix/issues/).**
 static UNSANITARY_OBJECTS: &[&str] = &["interface"];
 
 /// Check if a given struct's name contains any entry from the `UNSANITARY_OBJECTS` list.
+///
+/// # Parameters
+/// * `name: &str` - Struct name to check
+///
+/// # Returns
+///
+/// `true/false` depending if a match is found.
 pub fn is_unsanitary(name: &str) -> bool {
     UNSANITARY_OBJECTS
         .iter()
         .any(|&word| name.to_lowercase().contains(word))
+}
+
+#[cfg(test)]
+mod tests {
+	use super::*;
+
+	#[test]
+	fn test_exact_match() {
+		assert!(is_unsanitary("interface"));
+	}
+
+	#[test]
+	fn test_case_insensitive() {
+		assert!(is_unsanitary("INTERFACE"));
+		assert!(is_unsanitary("interface"));
+	}
+
+	#[test]
+	fn test_substring_match() {
+		assert!(is_unsanitary("interface_type"));
+		assert!(is_unsanitary("interface_name"));
+	}
+
+	#[test]
+	fn test_no_match() {
+		assert!(!is_unsanitary("device"));
+		assert!(!is_unsanitary(""));
+		assert!(!is_unsanitary("other"));
+	}
+
+	#[test]
+	fn test_whitespace() {
+		assert!(is_unsanitary(" interface "));
+		assert!(is_unsanitary("\tinterface\n"));
+	}
 }
